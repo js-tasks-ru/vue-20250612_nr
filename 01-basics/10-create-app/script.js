752cc572c00a6b4adfa70e14a58d5108b47e3756
Nikita Radkevich <nr@mna100.ru>
2025-06-16 23:22:37 +0300

    Task 01: create app

--- 01-basics/10-create-app/script.js
+++ 01-basics/10-create-app/script.js
@@ -1,1 +1,20 @@
 import { defineComponent, createApp } from 'vue'
+
+const App = defineComponent({
+  name: 'App',
+  setup() {
+    function getCurrentDate() {
+      return new Date().toLocaleDateString('en-US', {
+        month: 'long',
+        day: 'numeric',
+        year: 'numeric',
+      })
+    }
+
+    return { getCurrentDate };
+  },
+  template: `<div>Сегодня {{ getCurrentDate() }}</div>`,
+})
+
+const app = createApp(App)
+const vm = app.mount('#app')
